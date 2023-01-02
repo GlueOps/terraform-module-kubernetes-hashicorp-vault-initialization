38edf2e253fd1d4d4896328a84f5131657159537
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-01-02 04:01:30 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "vaultoperator" {
+  vault_addr = local.vault_url
+}
+
+resource "vaultoperator_init" "default" {
+  secret_shares    = 1
+  secret_threshold = 1
+}
+
+resource "local_sensitive_file" "foo" {
+  content  = jsonencode({ "vault_address" = "https://127.0.0.1:8200", "root_token" = "${vaultoperator_init.default.root_token}", "keys" = "${vaultoperator_init.default.keys}" })
+  filename = "../vault_access.json"
+}
+
+resource "time_static" "unseal-vault" {
+  for_each = toset(["0", "1", "2"])
+  triggers = {
+    rerun = timestamp()
+  }
+  provisioner "local-exec" {
+    command = "sleep 30 && kubectl -n glueops-core-vault exec -it vault-${each.key} -- vault operator unseal ${vaultoperator_init.default.keys[0]}"
+  }
+  depends_on = [
+    vaultoperator_init.default,
+    time_static.unseal-vault["0"]
+  ]
+}
